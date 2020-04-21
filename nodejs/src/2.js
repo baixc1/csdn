338f35a50a6f176a21b38a34d8a10750d06072cf
baixc1 <863562441@qq.ocm>
2020-04-21 18:46:44 +0800

    11

--- nodejs/src/2.js
+++ nodejs/src/2.js
@@ -1,7 +1,19 @@
 const koa = require('koa')
+const parse = require('co-body')
+
 const app = new koa()
 
-app.use(async ctx => {
-    ctx.body = 'hello koa2'
+app.use(async (ctx, next) => {
+    const start = Date.now()
+    await next()
+    const end = Date.now()
+    console.log(end - start)
 })
+
+app.use(async ctx => {
+    ctx.body = ctx.url
+})
+
+app.use()
+
 app.listen(3000, () => console.log('server run'))
