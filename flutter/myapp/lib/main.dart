fc010bbf479736c4f2064267145c64f6e60f0ff2
baixc <xiaocheng.bai@31huiyi.com>
2020-08-07 09:14:59 +0800

    flutter

--- flutter/myapp/lib/main.dart
+++ flutter/myapp/lib/main.dart
@@ -11,69 +11,100 @@ void main() {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipTestRoute();
-  }
-}
-
-class ClipTestRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // 头像
-    Widget avatar = Image.asset("imgs/avatar.png", width: 60.0);
-    return Center(
-      child: Column(
-        children: <Widget>[
-          avatar, //不剪裁
-          ClipOval(child: avatar), //剪裁为圆形
-          ClipRRect(
-            //剪裁为圆角矩形
-            borderRadius: BorderRadius.circular(5.0),
-            child: avatar,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                widthFactor: .5, //宽度设为原来宽度一半，另一半会溢出
-                child: avatar,
-              ),
-              Text(
-                "你好世界",
-                style: TextStyle(color: Colors.green),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ClipRect(
-                //将溢出部分剪裁
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  widthFactor: .5, //宽度设为原来宽度一半
-                  child: avatar,
-                ),
-              ),
-              Text("你好世界", style: TextStyle(color: Colors.green))
-            ],
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(color: Colors.red),
-            child: ClipRect(
-                clipper: MyClipper(), //使用自定义的clipper
-                child: avatar),
-          )
-        ],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('text'),
       ),
+      body: Demos(),
     );
   }
 }
 
-class MyClipper extends CustomClipper<Rect> {
+class Demos extends StatelessWidget {
   @override
-  Rect getClip(Size size) => Rect.fromLTWH(10.0, 15.0, 40.0, 30.0);
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context).settings.arguments;
+    if (args == 'ScrollControllerTestRoute') {
+      return ScrollControllerTestRoute();
+    }
+
+    Widget cmBtn(page) {
+      return RaisedButton(
+        child: Text(page),
+        onPressed: () {
+          Navigator.pushNamed(context, "/", arguments: page);
+        },
+      );
+    }
+
+    return Column(
+      children: <Widget>[
+        cmBtn('ScrollControllerTestRoute'),
+      ],
+    );
+  }
+}
+
+class ScrollControllerTestRoute extends StatefulWidget {
+  @override
+  ScrollControllerTestRouteState createState() {
+    return new ScrollControllerTestRouteState();
+  }
+}
+
+class ScrollControllerTestRouteState extends State<ScrollControllerTestRoute> {
+  ScrollController _controller = new ScrollController();
+  bool showToTopBtn = false; //是否显示“返回到顶部”按钮
 
   @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) => false;
+  void initState() {
+    super.initState();
+    //监听滚动事件，打印滚动位置
+    _controller.addListener(() {
+      print(_controller.offset); //打印滚动位置
+      if (_controller.offset < 1000 && showToTopBtn) {
+        setState(() {
+          showToTopBtn = false;
+        });
+      } else if (_controller.offset >= 1000 && showToTopBtn == false) {
+        setState(() {
+          showToTopBtn = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    //为了避免内存泄露，需要调用_controller.dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("滚动控制")),
+      body: Scrollbar(
+        child: ListView.builder(
+            itemCount: 100,
+            itemExtent: 50.0, //列表项高度固定时，显式指定高度是一个好习惯(性能消耗小)
+            controller: _controller,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("$index"),
+              );
+            }),
+      ),
+      floatingActionButton: !showToTopBtn
+          ? null
+          : FloatingActionButton(
+              child: Icon(Icons.arrow_upward),
+              onPressed: () {
+                //返回到顶部时执行动画
+                _controller.animateTo(.0,
+                    duration: Duration(milliseconds: 200), curve: Curves.ease);
+              }),
+    );
+  }
 }
