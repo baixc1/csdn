3457e99efe639930818db9fd559904fdce76744f
baixc <xiaocheng.bai@31huiyi.com>
2020-08-21 20:33:03 +0800

    vue

---
+++ vue/vuejs实战/学习笔记/note4/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Response />
+  </div>
+</template>
+
+<script>
+import Response from './components/Response'
+
+export default {
+  name: 'App',
+  components: {
+    Response
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vuejs实战/学习笔记/note4/src/components/Response.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="example">{{message}}</div>
+</template>
+<script>
+// import Vue from 'vue'
+// const data = { a: 1, obj: {}, arr: [] }
+// var vm = new Vue({
+//   data
+// })
+// vm.b = 1
+// vm.$set(data.obj, 'c', 1)
+// vm.$set(data.arr, 1, 1)
+// console.log(data) // 下图1
+// console.log(vm)   // 下图2
+// console.log(data === vm.$data) // true
+
+export default {
+  data () {
+    return {
+      message: '123'
+    }
+  },
+  async mounted () {
+    this.message = 'new message' // 更改数据
+    console.log(this.$el.textContent === 'new message') // false
+    console.log(this.$data.message)
+    this.$nextTick(function () {
+      console.log(this.$el.textContent === 'new message') // true
+    })
+    await this.$nextTick()
+    console.log(this.$el.textContent === 'new message')
+  }
+}
+</script>
