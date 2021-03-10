479ed084e2f4e0b2f2f13681f82ebe4a0db748dd
baixc <xiaocheng.bai@31huiyi.com>
2021-03-10 11:37:20 +0800

    ssr react-router

---
+++ note/note22/route/src/client/components/UserList.js
@@ -0,0 +1,14 @@
+// src/client/components/UserList.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const UserList = () => {
+  return (
+    <div>
+      <div>User List</div>
+      <Link to="/">To home page</Link>
+    </div>
+  );
+};
+
+export default UserList;
---
+++ note/note22/route/src/index.js
@@ -0,0 +1,33 @@
+// src/index.js
+import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import Routes from "./client/Routes";
+
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  const content = renderToString(
+    <StaticRouter location={req.path}>
+      <Routes />
+    </StaticRouter>
+  );
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root">${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
