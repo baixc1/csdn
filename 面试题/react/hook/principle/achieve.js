060a574e8ecfcc77da816a7869ffa57e0cf4d4c5
baixc <xiaocheng.bai@31huiyi.com>
2021-04-09 12:27:47 +0800

    react hook

--- 面试题/react/hook/principle/achieve.js
+++ 面试题/react/hook/principle/achieve.js
@@ -1,3 +1,6 @@
+// 核心：全局数组，控制组件状态
+// 过程：初次渲染 -> 事件触发(set赋值，cursor=0, fn刷新) -> re Render
+
 let memoizedState = []; // hooks 存放在这个数组
 let cursor = 0; // 当前 memoizedState 下标
 
@@ -11,14 +14,17 @@ function useState(initialValue) {
   return [memoizedState[cursor++], setState]; // 返回当前 state，并把 cursor 加 1
 }
 
+let fun; // fn是销毁函数（简单模拟）
 function useEffect(callback, depArray) {
+  // 销毁之前函数
+  typeof fun === "function" && fun();
   const hasNoDeps = !depArray;
   const deps = memoizedState[cursor];
   const hasChangedDeps = deps
     ? !depArray.every((el, i) => el === deps[i])
     : true;
   if (hasNoDeps || hasChangedDeps) {
-    callback();
+    fun = callback();
     memoizedState[cursor] = depArray;
   }
   cursor++;
@@ -28,13 +34,13 @@ function fn() {
   const [count, setCount] = useState(0);
   const [username, setUsername] = useState("fan");
   useEffect(() => {
-    console.log(count);
-  }, [count]);
+    console.log("count", count); // 只执行一次
+  }, []);
   useEffect(() => {
-    console.log(username);
+    console.log("username", username);
   }, [username]);
-  console.log(cursor);
-  console.log(memoizedState);
+  console.log("cursor", cursor);
+  console.log("memoizedState", memoizedState);
 
   // 模拟按钮
   var btn = document.createElement("button");
