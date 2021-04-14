19eff0e8f45fafc8ff6baadadf64fb917801672d
baixc <xiaocheng.bai@31huiyi.com>
2021-04-14 15:12:32 +0800

    xss

--- 面试题/js/xss/demo2/server.js
+++ 面试题/js/xss/demo2/server.js
@@ -3,6 +3,8 @@ const Koa = require("koa");
 const app = new Koa();
 const route = require("koa-route");
 var bodyParser = require("koa-bodyparser");
+const KoaStatic = require("koa-static");
+app.use(KoaStatic(__dirname));
 const cors = require("@koa/cors");
 
 // 临时用一个变量来存储，实际应该存在数据库中
@@ -12,6 +14,16 @@ app.use(bodyParser()); // 处理post请求的参数
 
 // 在用户名的输入框输入 '<script>alert('存储型 XSS 攻击')</script>'
 const login = (ctx) => {
+  // 简单设置一个cookie
+  ctx.cookies.set("cid", "hello world", {
+    domain: "localhost", // 写cookie所在的域名
+    path: "/", // 写cookie所在的路径
+    maxAge: 10 * 60 * 1000, // cookie有效时长
+    expires: new Date("2021-02-15"), // cookie失效时间
+    httpOnly: true, // 是否只用于http请求中获取
+    overwrite: false, // 是否允许重写
+  });
+
   const req = ctx.request.body;
   const userName = req.userName;
   currentUserName = userName;
