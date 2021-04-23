22b160268e7a5b91fe3acc39c75e88328dbcfbde
baixc <xiaocheng.bai@31huiyi.com>
2021-04-23 17:05:41 +0800

    同步模块模式

--- DesignPatterns/Architecture/SyncModuleDefine(SMD)/index.js
+++ DesignPatterns/Architecture/SyncModuleDefine(SMD)/index.js
@@ -26,7 +26,47 @@ var F = function () {
     }
     return this;
   };
-  return { define };
+
+  /**
+   * 模块调用方法（获取对应的模块，传参给回调函数）
+   * 格式一：(['模块一','模块二'],function(模块一, 模块二){})
+   * 格式二：('模块一','模块二', function(模块一, 模块二){})
+   */
+  const module = function () {
+    let args = [].slice.call(arguments), // 参数列表
+      fn = args.pop(), // 回调函数
+      parts = Array.isArray(args[0]) ? args[0] : args, // 依赖模块，两种格式：数组或多个参数（call，apply）
+      modules = [], // 实际依赖模块列表
+      modIds = "", // 模块路由
+      i = 0, // 遍历依赖模块的指针
+      ilen = parts.length, // 依赖模块长度
+      parent, // 父模块
+      j, // 遍历路由指针
+      jlen; // 路由长度
+
+    while (i < ilen) {
+      // 处理字符串命令的模块
+      if (typeof parts[i] === "string") {
+        parent = this;
+        // 按点分割路由
+        modIds = parts[i].split(".");
+        // 遍历路由(取出最后层级上的模块方法)
+        for (j = 0, jlen = modIds.length; j < jlen; j++) {
+          // 重置父模块
+          parent = parent[modIds[j]];
+        }
+        modules.push(parent);
+      }
+      // 处理对象等
+      else {
+        modules.push(parts[i]);
+      }
+      i++;
+    }
+    // 调用回调函数，分配对应的模块参数
+    fn.apply(null, modules);
+  };
+  return { define, module };
 };
 
 const f = F();
@@ -39,7 +79,8 @@ f.define("string.format", function () {
     },
   };
 });
-// 定义
+
+// 定义命名空间
 f.define("dom", function () {
   const $ = function (id) {
     $.dom = document.getElementById(id);
@@ -48,6 +89,35 @@ f.define("dom", function () {
   $.html = function (html) {
     // 赋值
     if (html) {
+      this.com.innerHTML = html;
+      return this;
+    }
+    // 取值
+    else {
+      return this.dom.innerHTML;
     }
   };
+  return $;
+});
+
+console.log(f.dom("test").html()); // test code
+
+// 命名空间下添加对象
+f.define("dom.addClass");
+// 重写方法
+f.dom.addClass = function (className) {
+  // 不存在class
+  if (!~this.dom.className.indexOf(className)) {
+    this.dom.className += " " + className;
+  }
+};
+
+f.dom("test").addClass("test");
+// 获取dom模块和document模块
+f.module(["dom", document], function (dom, doc) {
+  console.log(dom, doc);
+});
+// 获取dom和trim模块
+f.module("dom", "string.format.trim", function (dom, trim) {
+  console.log(dom, trim);
 });
